ac10f4bfe279534c9a99c078bc7386019f8f285d
Sebastian Schmidt <schmidtseb@outlook.de>
2019-11-08 19:41:13 +0100

    add sidenav + toolbar

--- frontend/issue-tracker/src/app/app.module.ts
+++ frontend/issue-tracker/src/app/app.module.ts
@@ -4,15 +4,27 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule, MatSidenavModule, MatToolbar, MatToolbarModule, MatButtonModule, MatIconModule } from '@angular/material/';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    LayoutModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
