55d44be48c275b194aac29a0acf8f9249faa3f42
Sebastian Schmidt <schmidtseb@outlook.de>
2019-12-03 17:56:21 +0100

    avoid setting content-type manually

--- frontend/issue-tracker/src/app/shared/interceptors/auth.interceptor.ts
+++ frontend/issue-tracker/src/app/shared/interceptors/auth.interceptor.ts
@@ -8,13 +8,10 @@ export class AuthInterceptor {
   constructor() { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('request', request);
     const token: string = localStorage.getItem('token');
     if (token) {
       request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
-    }
-
-    if (!request.headers.has('Content-Type')) {
-      request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
     }
 
     request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
