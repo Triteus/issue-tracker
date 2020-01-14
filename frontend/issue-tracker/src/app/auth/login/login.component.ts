b3e820bb944bb3b518b764ae13edc47e149e7f3d
Sebastian Schmidt <schmidtseb@outlook.de>
2020-01-14 20:32:08 +0100

    Add option to login with visitor account

--- frontend/issue-tracker/src/app/auth/login/login.component.ts
+++ frontend/issue-tracker/src/app/auth/login/login.component.ts
@@ -13,11 +13,22 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService, private snackBar: MatSnackBar, private router: Router) { }
 
+  visitorAccHelpMsg = 'Mit dem Testaccount können alle Daten eingesehen werden. ' +
+                      'Jegliche Aktionen wie das Löschen oder Erstellen von Tickets sind deaktiviert.';
+
   ngOnInit() {
   }
 
   onSubmit(form: NgForm) {
-    const {email, password} = form.form.value;
+    const { email, password } = form.form.value;
+    this.login(email, password);
+  }
+
+  loginAsVisitor() {
+    this.login('randomvisitor@mail.com', 'visitor');
+  }
+
+  private login(email: string, password: string) {
     this.authService.login(email, password).subscribe(res => {
       this.snackBar.open('Anmeldung erfolgreich!', 'OK');
       this.router.navigateByUrl('/home');
