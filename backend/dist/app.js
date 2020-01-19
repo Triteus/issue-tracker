745d2213080471f5d980df7dee5b59d1bb21beba
Sebastian Schmidt <schmidtseb@outlook.de>
2020-01-19 01:35:56 +0100

    Fix port

--- backend/dist/app.js
+++ backend/dist/app.js
@@ -1,10 +1,6 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 const AppServer_1 = require("./AppServer");
-const config_1 = __importDefault(require("config"));
 const server = new AppServer_1.AppServer();
-server.start(config_1.default.get('serverPort'));
+server.start(parseInt(process.env.PORT));
 //# sourceMappingURL=app.js.map
